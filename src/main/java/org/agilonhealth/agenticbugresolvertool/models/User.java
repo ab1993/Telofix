8b3a31ccdc8658f294758c4c23664f50b7ea5cb4
Telofix AI <ai-agent@telofix.com>
2026-04-11 16:46:27 +0000

    Fix SCRUM-10: Add @Document annotation to User model for MongoDB mapping

--- src/main/java/org/agilonhealth/agenticbugresolvertool/models/User.java
+++ src/main/java/org/agilonhealth/agenticbugresolvertool/models/User.java
@@ -5,6 +5,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@Document(collection = "users")
 public class User {
     @Id
     private Long id;
