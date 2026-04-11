e7ddf0c667d3aa00f1c4839e7bfc46bfdf523952
Abhishek Sharma Owner <absharmam@gmail.com>
2026-04-11 21:26:55 +0530

    testing

--- src/main/java/org/agilonhealth/agenticbugresolvertool/models/User.java
+++ src/main/java/org/agilonhealth/agenticbugresolvertool/models/User.java
@@ -4,7 +4,6 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "users")
 @Data
 public class User {
     @Id
